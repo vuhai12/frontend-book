81693df3ceea059f387b3b5c758bb851ed73b34b
gotechdevweb <vuhai91az@gmail.com>
2025-02-13 15:40:27 +0700

    push code

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -109,7 +109,7 @@ const Login = () => {
           <input
             type="password"
             autoComplete="off"
-            name="password"
+            // name="password"
             className={`w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 ${
               errors.password ? "border-red-500" : "border-gray-300"
             }`}
