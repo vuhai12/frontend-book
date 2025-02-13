471dd0003d5a5ab18b7dcd9ae9cc563e2f87ae83
gotechdevweb <vuhai91az@gmail.com>
2025-02-13 15:45:07 +0700

    push code

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -90,7 +90,7 @@ const Login = () => {
           </label>
           <input
             type="email"
-            autoComplete="off"
+            autoComplete="new-password"
             className={`w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 ${
               errors.email ? "border-red-500" : "border-gray-300"
             }`}
@@ -108,8 +108,8 @@ const Login = () => {
           </label>
           <input
             type="password"
-            autoComplete="off"
-            // name="password"
+            autoComplete="new-password"
+            name="password"
             className={`w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 ${
               errors.password ? "border-red-500" : "border-gray-300"
             }`}
